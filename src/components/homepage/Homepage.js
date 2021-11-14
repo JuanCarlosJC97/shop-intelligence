78c6486f7c01fec4204e6ac30ab6495ea23ba34c
Charly <juancarlos.juarez97@outlook.com>
2021-11-13 22:23:15 -0600

    modificación de la pantalla de home

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './Homepage.css';
 import { Button } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
-import Logo2 from '../../images/logo2.svg'
+import Logo2 from '../../images/logo2.svg';
+import Mark from '../../images/pin2.png';
+import Search from '../../images/loupe.png';
+import ShopCart from '../../images/shopping-cart.png';
 
 
 export default function Homepage() {
@@ -21,11 +24,38 @@ export default function Homepage() {
                     <Button id="nosotros" className="mb-3" type="button" variant="links" size="lg" bsPrefix="btn" >Nosotros</Button>
                     </div>
                     <div className="icons">
-
+                        <Image className="mark" src={Mark}></Image>
+                        <Image className="search" src={Search}></Image>
+                        <Image className="shopCart" src={ShopCart}></Image>
                     </div>
                 </div>
             </div>
             <div className="body">
+                <div className="colum1">
+                    <span className="text1">¡ Ven por nuestro</span>
+                    <span className="text2">Pan de muerto Tradicional!</span>
+                    <div className="promo">
+                        <div className="promoText">
+                            <p className="promoTitle"></p>
+                            <div >
+                                <p className="promoCost">3 x 2 Pan de Muerto</p>
+                                <p className="promoTerm">Por $50</p>
+                                <p className="promoDesc">Marmoleado y sin azúcar añadida</p>
+                            </div>
+                            <Button></Button>
+                        </div>
+                        <div className="prooButtons">
+                            <Button></Button>
+                            <Button></Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="colum2">
+
+                </div>
+                <div className="colum3">
+
+                </div>
             </div>
             <div className="footer">
             </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Auth from './components/Auth';
+// import Auth from './components/Auth';
 // import Register from './components/register/Register';
 // import RestorePass from './components/restorePass/RestorePass';
 // import RecoverAccount from './components/recoverAccount/RecoverAccount';
-// import Homepage from './components/homepage/Homepage'
+import Homepage from './components/homepage/Homepage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 const textos = [
   { title: "Accede a tu cuenta", buttonUno: "Registrarse", buttonDos: "Iniciar Sesión", place1:"Nombre de usuario", place2: "Contraseña"}
@@ -17,10 +17,10 @@ const textos = [
 ReactDOM.render(
   <React.StrictMode>
     {/* <Register texts={textos}/> */}
-    <Auth texts={textos}/>
+    {/* <Auth texts={textos}/> */}
     {/* <RestorePass /> */}
     {/* <RecoverAccount /> */}
-    {/* <Homepage /> */}
+    <Homepage /> 
 
   </React.StrictMode>,
   document.getElementById('root')
